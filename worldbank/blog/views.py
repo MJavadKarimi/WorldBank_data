cd8cec299a635e7605e2d35c68306a22bf1e63bb
MJavadKarimi <j14k14@gmail.com>
2023-07-01 11:46:16 +0330

    create index.html template

--- worldbank/blog/views.py
+++ worldbank/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Indicators, Countries, Datas
 
 
 def index(request):
-    return HttpResponse("This is the index page.")
+    countries_number = Countries.objects.count()
+    context = {'countries_number': countries_number}
+    return render(request, 'index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
