3748f91254d9050e9912978fbd5d21dc8c3eb806
MJavadKarimi <j14k14@gmail.com>
2023-07-08 17:21:41 +0330

    change ALLOWED_HOSTS setting and add date and amount to comparison.html

--- worldbank/worldbank/settings.py
+++ worldbank/worldbank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%%5$7leta2vd9wn*+-)apdmjj8+vy2z-i4yh*4%3_hp-xxn%13
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
